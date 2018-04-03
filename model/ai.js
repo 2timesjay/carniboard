2d6e481676426bf46541cea06a237f28f442106b
Jacob Jensen <2timesjay@gmail.com>
2018-04-02 23:38:32 -0400

    rollout utilities (untested)

--- model/ai.js
+++ model/ai.js
@@ -3,13 +3,40 @@ Array.prototype.flatMap = function (lambda) {
 }
 
 score = function(state) {  // (state: State): number 
-    return state.score
+    return state.score;
 }
 
-policy = function(state) {  // (state: State): number[]
+policy = function(selections) {  // (selections: selection[]): number[]
+    if (selections.length == 0) { return []; }
+    let random_policy = new Array(selections.length);
+    random_policy.fill(1.0/selections.length);
+    return random_policy;
 }
 
-score
+selectFromDistribution = function(distribution) {
+    rand = Math.random();
+    current = 0;
+    for(var i = 0; i < distribution.length; i++) {
+        current += distribution[i];
+        if (current > rand) {
+            return i;
+        }
+    }
+    return i;
+}
+
+rollout = function(state) {
+    let max_turns = 50;
+    let cur_turn = 0;
+    let rollout_state = state;
+    while (cur_turn < max_turns) {
+        cur_turn++;
+        let selections = generateAllSelections(state);
+        let policy = policy(selections);
+        let selection = selections[selectFromDistribution(policy)];
+        rollout_state = executeStacks(state, [selection])
+    }
+}
 
 executeStacks = function(state, stacks) {
     let clone = state.clone();
